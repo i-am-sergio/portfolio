9a7c54643888f7c60b6620391f4276fb985ead57
i-am-sergio <sergio15.dev@gmail.com>
2025-03-16 17:09:21 -0500

    frontend: redesign scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scrollbar scrollbar-track-primary scrollbar-thumb-accent">
       <body
         className={`${jetbrainsMono.variable} antialiased`}
       >
