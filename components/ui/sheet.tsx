7ef6fa42e753cadc1006a2795f4376f388cd38ad
i-am-sergio <sergio15.dev@gmail.com>
2025-03-16 16:38:16 -0500

    refactor: upgrade tailwindcss to version 4

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -64,7 +64,7 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetPrimitive.Close className="absolute right-8 top-8 transition-opacity outline-none"> 
+      <SheetPrimitive.Close className="absolute right-8 top-8 transition-opacity outline-hidden"> 
         <IoMdClose className="text-3xl text-accent" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
